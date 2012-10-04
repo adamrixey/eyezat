573c093df5e671a176abe1d0b9a5f45487478f7e
Adam Rixey <adam.rixey@wavara.com>
2012-10-04 12:28:55 -0400

    Break out views from a tweaked single eyezat.erb.

--- eyezat.rb
+++ eyezat.rb
@@ -1,24 +1,36 @@
 # http://www.colourlovers.com/palette/1/metro
 # https://developer.mozilla.org/en-US/docs/Using_geolocation
 # http://dev.w3.org/geo/api/spec-source.html
+# https://developers.google.com/maps/documentation/javascript/geocoding#Geocoding
 
 require 'rubygems'
 require 'sinatra'
 Dir[File.dirname(__FILE__)+'/models/*.rb'].each { |f| require f }
 
 get '/' do
-  @link = ''
-  @coordinates = { :latitude => '', :longitude => '' }
-  @map_zoom = 16
-  erb :eyezat
+  @title = 'eyez.at: Geocoordinate link shortener'
+  @init_func = 'initMain'
+  erb :main
+end
+
+get '/about/?' do
+  @title = 'eyez.at: About the geocoordinate link shortener'
+  erb :about
+end
+
+get '/alt/?' do
+  @title = 'eyez.at: Non-current location geocoordinate link shortener'
+  @init_func = 'initAlt'
+  erb :alt
 end
 
 get '/:link' do
+  @title = 'eyez.at this geocoordinate location'
+  @init_func = 'initLink'
   @link = params[ :link ]
   @coordinates = Geojam.new( @link ).coordinates
-  @map_zoom = 13
   raise Sinatra::NotFound unless @coordinates
-  erb :eyezat
+  erb :link
 end
 
 not_found do
